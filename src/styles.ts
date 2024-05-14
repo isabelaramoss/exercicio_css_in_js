4ef6d601b2691cdb7bb75341e3674fbd6e453d55
Isabela Ramos <isamrteixeira@gmail.com>
2024-05-14 12:25:43 -0300

    arrumando container global

--- src/styles.ts
+++ src/styles.ts
@@ -1,36 +1,32 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
 const EstiloGlobal = createGlobalStyle`
-:root {
-  --cor-principal: #a7727d;
-  --cor-secundaria: #f9f5e7;
-}
+  :root {
+    --cor-principal: #a7727d;
+    --cor-secundaria: #f9f5e7;
+  }
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: Lato, sans-serif;
-  list-style: none;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Lato, sans-serif;
+    list-style: none;
+  }
 
-body {
-  padding-bottom: 120px;
-}
+  body {
+    padding-bottom: 120px;
+  }
 
 `
 export default EstiloGlobal
 
 export const Container = styled.div`
-  .container {
-    max-width: 1024px;
-    width: 100%;
-    margin: 0 auto;
-  }
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 
   @media (max-width: 1024px) {
-    .container {
-      max-width: 80%;
-    }
+    max-width: 80%;
   }
 `
